0c7e5a8619fc5b0e8d709f6bef943be6fecd0ab5
Vyankatesh Khandke <khandke.vyankatesh@gmail.com>
2023-12-19 09:40:31 +0530

    Update

--- React_Start/FirstProject_PizzaApp_Final/pizza-menu/src/index.js
+++ React_Start/FirstProject_PizzaApp_Final/pizza-menu/src/index.js
@@ -6,12 +6,12 @@ import ReactDOM from "react-dom/client";
 // Create App Component
 
 function App() {
+  //   const x = "Jonas"; // EsLint gives warning
   return <h1>Hello React!</h1>;
 }
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 // Activating Strict Mode
 root.render(
   <React.StrictMode>
