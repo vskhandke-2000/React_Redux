b3d73b32018a5be1f5932a6eb6c959070777ef76
Vyankatesh Khandke <khandke.vyankatesh@gmail.com>
2023-12-25 17:43:50 +0530

    Update App.js

--- 06_eat-n-split/src/App.js
+++ 06_eat-n-split/src/App.js
@@ -18,3 +18,34 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+    </li>
+  );
+}
