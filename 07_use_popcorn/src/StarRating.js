93b6ea8700a1f3c8a56577df7dcf9c2c6326b56d
Vyankatesh Khandke <khandke.vyankatesh@gmail.com>
2024-01-17 10:29:46 +0530

    Star Rating Update

--- 07_use_popcorn/src/StarRating.js
+++ 07_use_popcorn/src/StarRating.js
@@ -12,12 +12,14 @@ const starContainerStyle = {
   //   gap: "4px",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#FCC419",
+  strokeColor = "#D30000",
+  size = 48,
+  className = "",
+  msg = [],
+}) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
 
@@ -25,8 +27,15 @@ export default function StarRating({ maxRating = 5 }) {
     setRating(rating);
   }
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -35,23 +44,39 @@ export default function StarRating({ maxRating = 5 }) {
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            strokeColor={strokeColor}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {msg.length === maxRating
+          ? msg[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
+      {/* <p style={textStyle}>{tempRating || rating || ""}</p> */}
       {/* <p style={textStyle}>{tempRating ? tempRating : rating || ""}</p> */}
     </div>
   );
 }
 
-const startStyle = {
-  width: "48px",
-  height: "48px",
-  dispaly: "block",
-  cursor: "pointer",
-};
+function Star({
+  onRate,
+  fullStar,
+  onHoverIn,
+  onHoverOut,
+  color,
+  strokeColor,
+  size,
+}) {
+  const startStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    dispaly: "block",
+    cursor: "pointer",
+  };
 
-function Star({ onRate, fullStar, onHoverIn, onHoverOut }) {
   return (
     <span
       role="button"
@@ -64,8 +89,8 @@ function Star({ onRate, fullStar, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#0FF"
-          stroke="#F00"
+          fill={color}
+          stroke={strokeColor}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -74,7 +99,7 @@ function Star({ onRate, fullStar, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#F00"
+          stroke={strokeColor}
         >
           <path
             strokeLinecap="round"
--- 07_use_popcorn/src/index.js
+++ 07_use_popcorn/src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// import StarRating from "./StarRating";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      size={64}
+      color="#00F"
+      strokeColor="#F00"
+      className="test"
+    />
+    <StarRating
+      maxRating={5}
+      size={64}
+      color="#00F"
+      strokeColor="#F00"
+      className="test"
+      msg={["Terrible", "Bad", "Good", "Great", "Awesome"]}
+    /> */}
   </React.StrictMode>
 );
